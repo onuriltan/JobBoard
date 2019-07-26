b81fb709a5b866dee5db46779bd9f19329f68e4e
onuriltan <onur.iltan@gmail.com>
2019-07-27 00:03:34 +0300

    add sample schema and resolvers to express server

---
+++ server/resolvers.js
@@ -0,0 +1,6 @@
+const Query = {
+  greeting: () => 'Hello World!'
+};
+
+module.exports = { Query };
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
 const express = require('express');
 const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
@@ -16,11 +17,14 @@ app.use(cors(), bodyParser.json(), expressJwt({
   credentialsRequired: false
 }));
 
-const typeDefs = '';
-const resolvers = {};
+// Load Type Definitions
+const typeDefs = fs.readFileSync('./schema.graphql', {encoding: 'utf-8'});
+// Load Resolvers
+const resolvers = require('./resolvers');
+// Make Schema
 const schema = makeExecutableSchema( { typeDefs, resolvers });
 app.use('/graphql', graphqlExpress({schema}));
-app.use('/grapihql', graphiqlExpress({endpointURL: '/graphql'}));
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
 
 
