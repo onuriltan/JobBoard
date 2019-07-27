0f97831a3a7cc91381357cdf67740df8dd4fbbd3
onuriltan <onur.iltan@gmail.com>
2019-07-27 23:15:58 +0300

    add loadJob function to fetch job

--- client/src/requests.js
+++ client/src/requests.js
@@ -20,3 +20,26 @@ export async function loadJobs() {
   const responseBody = await response.json();
   return responseBody.data.jobs;
 }
+
+export async function loadJob(id) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({
+      query: `query JobQuery($id: ID!){
+        job(id:$id) {
+          id
+          title
+          description
+          company {
+            id
+            name
+          }
+        }
+      }`,
+      variables: { id }
+    })
+  });
+  const responseBody = await response.json();
+  return responseBody.data.job;
+}
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 
 const Query = {
-  job: (root, args) => db.jobs.get(args.id),
+  job: (root, { id }) => db.jobs.get(id),
   jobs: () => db.jobs.list()
 };
 
