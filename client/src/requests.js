2d68e77de887f9c97fd2a6eb2139b05f1f5bbf1a
onuriltan <onur.iltan@trendyol.com>
2019-08-01 17:39:08 +0300

    add authorization header to ApolloClient

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,33 +1,30 @@
 import { getAccessToken, isLoggedIn } from "./auth";
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
+import { ApolloClient, HttpLink, ApolloLink, InMemoryCache } from 'apollo-boost'
 import gql from 'graphql-tag'
 
 const endpointURL = 'http://localhost:9000/graphql/';
 
+const authLink = new ApolloLink((operation, forward) => {
+  // set auth header if is loggedIn
+  if(isLoggedIn()){
+    operation.setContext({
+      headers: {
+        'authorization': `Bearer ${getAccessToken()}`
+      }
+    });
+  }
+  return forward(operation)
+});
+
 const client = new ApolloClient({
   // specify how to connect to the client(Http here)
-  link: new HttpLink({ uri: endpointURL }),
+  link: ApolloLink.from([
+    authLink, // before preparing http request look authLink for setting auth header
+    new HttpLink({ uri: endpointURL })
+    ]),
   // cache requests
   cache: new InMemoryCache()
 });
-
-async function graphqlRequest(query, variables) {
-  const request = {
-    method: 'POST',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify({ query, variables })
-  };
-  if(isLoggedIn()){
-    request.headers['authorization'] = `Bearer ${getAccessToken()}`
-  }
-  const response = await fetch(endpointURL, request);
-  const responseBody = await response.json();
-  if (responseBody.errors) {
-    const message = responseBody.errors.map(error => error.msg).join('\n');
-    throw new Error(message)
-  }
-  return responseBody.data;
-}
 
 export async function loadJobs() {
   const query = gql `{
