d0857dae90184289c9dc3ebe7e264d6536e4700c
onuriltan <onur.iltan@gmail.com>
2019-08-03 14:54:47 +0300

    save the createdJob to the cache so we dont need to call loadJob again

--- client/src/requests.js
+++ client/src/requests.js
@@ -6,7 +6,7 @@ const endpointURL = 'http://localhost:9000/graphql/';
 
 const authLink = new ApolloLink((operation, forward) => {
   // set auth header if is loggedIn
-  if(isLoggedIn()){
+  if (isLoggedIn()) {
     operation.setContext({
       headers: {
         'authorization': `Bearer ${getAccessToken()}`
@@ -21,72 +21,90 @@ const client = new ApolloClient({
   link: ApolloLink.from([
     authLink, // before preparing http request look authLink for setting auth header
     new HttpLink({ uri: endpointURL })
-    ]),
+  ]),
   // cache requests
   cache: new InMemoryCache()
 });
 
 export async function loadJobs() {
-  const query = gql `{
-        jobs {
+  const query = gql`{
+      jobs {
           id
           title
           company{
-            id
-            name
+              id
+              name
           }
-        }
-      }`;
+      }
+  }`;
   // fetch policy no cache means it will be send to server every time
   // because we need to query each time loadJobs called because a new job could be created by someone else
-  const { data: { jobs } } = await client.query( { query, fetchPolicy:'no-cache' });
+  const { data: { jobs } } = await client.query({ query, fetchPolicy: 'no-cache' });
   return jobs
 }
 
-export async function loadJob(id) {
-  const query = gql`
-      query JobQuery($id: ID!){
+const loadJobQuery = gql`
+    query JobQuery($id: ID!){
         job(id:$id) {
-          id
-          title
-          description
-          company {
             id
-            name
-          }
+            title
+            description
+            company {
+                id
+                name
+            }
         }
-      }`;
-  const { data: { job } } = await client.query( { query, variables: { id } });
+    }`;
+
+export async function loadJob(id) {
+  const { data: { job } } = await client.query({ query: loadJobQuery, variables: { id } });
   return job
 }
 
 export async function loadCompany(id) {
   const query = gql`query CompanyQuery($id:ID!){
-                    company(id: $id){
-                      name
-                      description
-                      jobs {
-                        id
-                        title
-                      }
-                    }
-                  }`;
-  const { data: { company } } = await client.query( { query, variables: { id } });
+      company(id: $id){
+          name
+          description
+          jobs {
+              id
+              title
+          }
+      }
+  }`;
+  const { data: { company } } = await client.query({ query, variables: { id } });
   return company
 }
 
 export async function createJob(input) {
-  const mutation = gql `mutation CreateJob($input: CreateJobInput){
-                  job: createJob(input: $input) {
-                    id
-                    title
-                    company {
-                      id
-                      name
-                    }
-                  }
-                }`;
-  const { data: { job } } = await client.mutate( { mutation, variables: { input } });
+  const mutation = gql`mutation CreateJob($input: CreateJobInput){
+      job: createJob(input: $input) {
+          id
+          title
+          description
+          company {
+              id
+              name
+          }
+      }
+  }`;
+  const { data: { job } } = await client.mutate({
+    mutation,
+    variables: { input } ,
+    /*
+     update will be called after mutation has been executed
+     tell Apollo Client whenever run the current mutation take the data from returned response
+     and save it to the cache as if it was the result of running createJobQuery for that specific job id
+     So that we dont need to call loadJob when we are browsed to that jobs page
+    */
+    update: (cache, { data }) => {
+      cache.writeQuery({
+        query: loadJobQuery,
+        variables:{ id: data.job.id },
+        data
+      })
+    }
+  });
   return job
 }
 
